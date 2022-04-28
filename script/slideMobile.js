e135b38097b7949b09328bf6ea78e917549c207f
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-04-28 17:55:13 -0300

    ajuste layout mobile header

--- script/slideMobile.js
+++ script/slideMobile.js
@@ -10,6 +10,19 @@ function nextImage() {
     if(count > 4) {
         count = 1;
     }
+
     document.getElementById("radio" + count).checked = true;
+
+    let tagTitle = document.getElementById("tagTitle")
+    let title = document.getElementById("title")
+
+    if(count === 1 || count === 3) {
+        tagTitle.innerHTML = "HUMANO VIRTUAL"
+        title.innerHTML = "Conheça o Papai Noel Virtual desenvolvido pela PlanXP"
+    }
+    if(count === 2 || count === 4) {
+        tagTitle.innerHTML = "DHL"
+        title.innerHTML = "Treinamento em realidade virtual na DHL"
+    }
 
 }
