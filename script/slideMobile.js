77cb6e70acfa7a6156b151f1f0fdb1185996e87b
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-04-29 12:48:37 -0300

    adicionado click com troca de textos para slide mobile

--- script/slideMobile.js
+++ script/slideMobile.js
@@ -1,7 +1,10 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function() {
+let tagTitle = document.getElementById("tagTitle")
+let title = document.getElementById("title")
+
+const slideInterval = setInterval( function() {
     nextImage();
 }, 5000)
 
@@ -13,8 +16,7 @@ function nextImage() {
 
     document.getElementById("radio" + count).checked = true;
 
-    let tagTitle = document.getElementById("tagTitle")
-    let title = document.getElementById("title")
+    
 
     if(count === 1 || count === 3) {
         tagTitle.innerHTML = "HUMANO VIRTUAL"
@@ -26,5 +28,22 @@ function nextImage() {
         title.innerHTML = "Treinamento em realidade virtual na DHL"
         document.getElementById("blockBox").src="./imagens//mobile/blockBorder2.png"
     }
+
+}
+
+function openSlide1() {
+    tagTitle.innerHTML = "HUMANO VIRTUAL"
+    title.innerHTML = "Conheça o Papai Noel Virtual desenvolvido pela PlanXP"
+    document.getElementById("blockBox").src="./imagens//mobile/blockBorder1.png"
+
+    clearInterval(slideInterval)
+}
+
+function openSlide2() {
+    tagTitle.innerHTML = "DHL"
+    title.innerHTML = "Treinamento em realidade virtual na DHL"
+    document.getElementById("blockBox").src="./imagens//mobile/blockBorder2.png"
+
+    clearInterval(slideInterval)
 
 }
